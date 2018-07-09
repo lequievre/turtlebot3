d484697543af50281591a7de4a95562f4a039965
lequievre <laurent.lequievre@gmail.com>
2018-07-09 16:01:56 +0200

    Adding first version of installation bash script of turtlebot3

---
+++ install_turtlebot3_part1.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+# Laurent LEQUIEVRE
+# laurent.lequievre@uca.fr
+# UMR 6602 - Institut Pascal
+# PART 1
+
+read -n1 -rsp $'Press any key to update and upgrade linux or Ctrl+C to exit...\n'
+
+# Update and Upgrade Linux
+sudo apt-get update
+sudo apt-get upgrade
+
+read -n1 -rsp $'Press any key to install synaptic and git or Ctrl+C to exit...\n'
+
+# Install synaptic
+sudo apt-get install synaptic
+sudo apt-get install git
+
+read -n1 -rsp $'Press any key to install ssh or Ctrl+C to exit...\n'
+
+#Install ssh
+
+#Install linux package
+sudo apt-get install openssh-server
+#Activate service ssh
+sudo systemctl enable ssh.service
+#Verify sate of ssh service
+sudo systemctl status ssh.service
+
+read -n1 -rsp $'Press any key to install ros kinetic or Ctrl+C to exit...\n'
+
+#Install ros kinetic
+cd ~
+mkdir install_ros_kinetic
+cd install_ros_kinetic
+wget https://raw.githubusercontent.com/ROBOTIS-GIT/robotis_tools/master/install_ros_kinetic.sh && chmod 755 ./install_ros_kinetic.sh && bash ./install_ros_kinetic.sh
+
+read -n1 -rsp $'Press any key to install ros kinetic dependent packages or Ctrl+C to exit...\n'
+
+# Install ros kinetic dependent packages
+sudo apt-get install ros-kinetic-rosserial-python ros-kinetic-tf
+
+read -n1 -rsp $'Press any key to install camera RealSense packages or Ctrl+C to exit...\n'
+
+#Install RealSense packages
+sudo apt-get install linux-headers-generic
+sudo apt-get install ros-kinetic-librealsense
+
+read -n1 -rsp $'Press any key to install turtlebot3 dependent packages or Ctrl+C to exit...\n'
+
+#Install turtlebot3 dependent packages
+cd ~
+mkdir -p catkin_ws/src
+cd catkin_ws/src
+git clone https://github.com/ROBOTIS-GIT/hls_lfcd_lds_driver.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+cd turtlebot3
+sudo rm -r turtlebot3_description/ turtlebot3_teleop/ turtlebot3_navigation/ turtlebot3_slam/ turtlebot3_example/
+cd ..
+git clone https://github.com/intel-ros/realsense.git
+cd realsense
+git checkout 1.8.0
+
+sudo reboot
+
+
+
---
+++ install_turtlebot3_part2.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Laurent LEQUIEVRE
+# laurent.lequievre@uca.fr
+# UMR 6602 - Institut Pascal
+# PART 2
+
+#Ip ros hostname and ros master uri and turtlebot3 model
+export IP_RHN=10.42.0.5
+export IP_RMU=10.42.0.1
+export T_MODEL=waffle
+
+read -n1 -rsp $'Press any key to catkin_make turtlebot3 packages or Ctrl+C to exit...\n'
+
+cd ~
+cd ~/catkin_ws && catkin_make -j2
+
+read -n1 -rsp $'Press any key to add .bashrc config or Ctrl+C to exit...\n'
+
+#Add devel bash script in .bashrc file
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
+#Add ROS_MASTER_URI and ROS_HOSTNAME
+echo "export ROS_MASTER_URI=http://$IP_RMU:11311" >> ~/.bashrc
+echo "export ROS_HOST_NAME=$IP_RHN" >> ~/.bashrc
+echo "export TURTLEBOT3_MODEL=$T_MODEL" >> ~/.bashrc
+echo "alias tb3_bringup='roslaunch turtlebot3_bringup turtlebot3_robot.launch'" >> ~/.bashrc
+
+source ~/.bashrc
+
+read -n1 -rsp $'Press any key to do USB settings or Ctrl+C to exit...\n'
+
+#USB settings
+rosrun turtlebot3_bringup create_udev_rules
+
+read -n1 -rsp $'Press any key to do setup OpenCR Update or Ctrl+C to exit...\n'
+
+#OpenCR setup
+export OPENCR_PORT=/dev/ttyACM0
+export OPENCR_MODEL=$T_MODEL
+
+cd ~
+mkdir ~/opencr_update
+cd opencr_update
+wget https://github.com/ROBOTIS-GIT/OpenCR/raw/develop/arduino/opencr_release/shell_update/opencr_update.tar.bz2 && tar -xvf opencr_update.tar.bz2 && cd ./opencr_update && ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr && cd ..
+
+
