bca5a0c45024c206a781caf7a5f795e7f33830ee
Laurent LEQUIEVRE <laurent.lequievre@gmail.com>
2018-07-10 14:56:28 +0200

    Removing read command, adding flashback session and rosdep update

--- install_turtlebot3_part1.sh
+++ install_turtlebot3_part1.sh
@@ -4,19 +4,20 @@
 # UMR 6602 - Institut Pascal
 # PART 1
 
-read -n1 -rsp $'Press any key to update and upgrade linux or Ctrl+C to exit...\n'
+#read -n1 -rsp $'Press any key to update and upgrade linux or Ctrl+C to exit...\n'
 
 # Update and Upgrade Linux
 sudo apt-get update
 sudo apt-get upgrade
 
-read -n1 -rsp $'Press any key to install synaptic and git or Ctrl+C to exit...\n'
+#read -n1 -rsp $'Press any key to install synaptic and git or Ctrl+C to exit...\n'
 
 # Install synaptic
 sudo apt-get install synaptic
 sudo apt-get install git
+sudo apt-get install gnome-session-flashback
 
-read -n1 -rsp $'Press any key to install ssh or Ctrl+C to exit...\n'
+#read -n1 -rsp $'Press any key to install ssh or Ctrl+C to exit...\n'
 
 #Install ssh
 
@@ -27,7 +28,7 @@ sudo systemctl enable ssh.service
 #Verify sate of ssh service
 sudo systemctl status ssh.service
 
-read -n1 -rsp $'Press any key to install ros kinetic or Ctrl+C to exit...\n'
+#read -n1 -rsp $'Press any key to install ros kinetic or Ctrl+C to exit...\n'
 
 #Install ros kinetic
 cd ~
@@ -35,18 +36,21 @@ mkdir install_ros_kinetic
 cd install_ros_kinetic
 wget https://raw.githubusercontent.com/ROBOTIS-GIT/robotis_tools/master/install_ros_kinetic.sh && chmod 755 ./install_ros_kinetic.sh && bash ./install_ros_kinetic.sh
 
-read -n1 -rsp $'Press any key to install ros kinetic dependent packages or Ctrl+C to exit...\n'
+#read -n1 -rsp $'Press any key to install ros kinetic dependent packages or Ctrl+C to exit...\n'
 
 # Install ros kinetic dependent packages
 sudo apt-get install ros-kinetic-rosserial-python ros-kinetic-tf
 
-read -n1 -rsp $'Press any key to install camera RealSense packages or Ctrl+C to exit...\n'
+#read -n1 -rsp $'Press any key to install camera RealSense packages or Ctrl+C to exit...\n'
 
 #Install RealSense packages
 sudo apt-get install linux-headers-generic
 sudo apt-get install ros-kinetic-librealsense
 
-read -n1 -rsp $'Press any key to install turtlebot3 dependent packages or Ctrl+C to exit...\n'
+#ros dep update
+rosdep update
+
+#read -n1 -rsp $'Press any key to install turtlebot3 dependent packages or Ctrl+C to exit...\n'
 
 #Install turtlebot3 dependent packages
 cd ~
@@ -61,6 +65,8 @@ cd ..
 git clone https://github.com/intel-ros/realsense.git
 cd realsense
 git checkout 1.8.0
+
+read -n1 -rsp $'Press any key to REBOOT or Ctrl+C to exit...\n'
 
 sudo reboot
 
