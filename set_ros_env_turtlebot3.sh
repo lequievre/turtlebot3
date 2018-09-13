acc298de9ef0557d53a3dd32b6653cd6f602785e
lequievre <laurent.lequievre@gmail.com>
2018-09-13 17:59:43 +0200

    Adding bash setup and modifying ip address

--- set_ros_env_turtlebot3.sh
+++ set_ros_env_turtlebot3.sh
@@ -8,8 +8,9 @@
 # Ip address of burger donatello -> 10.42.0.3
 # Ip address of waffle leonardo -> 10.42.0.4
 
-export ROS_MASTER_URI=http://10.42.0.1:11311
-export ROS_HOSTNAME=10.42.0.1
+export ROS_MASTER_URI=http://192.168.0.1:11311
+export ROS_HOSTNAME=192.168.0.1
 export TURTLEBOT3_MODEL=$1
 
+source ~/turtlebot3/catkin_ws/devel/setup.bash
 
