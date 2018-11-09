052ce1774fa8804e24361208fbc844f044c8bfd9
lequievre <laurent.lequievre@gmail.com>
2018-11-09 17:04:17 +0100

    Adding ros hostname and set to localhost

--- set_ros_env_local_turtlebot3.sh
+++ set_ros_env_local_turtlebot3.sh
@@ -6,6 +6,7 @@
 # UMR 6602 - Institut Pascal
 
 export ROS_MASTER_URI=http://localhost:11311
+export ROS_HOSTNAME=localhost
 export TURTLEBOT3_MODEL=$1
 
 source ~/turtlebot3/catkin_ws/devel/setup.bash
