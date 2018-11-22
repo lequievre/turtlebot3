09724e38951e6d61eedc8e1a88eddc8ceb415342
lequievre <laurent.lequievre@gmail.com>
2018-11-22 18:45:39 +0100

    Adding student installation

--- install_pc_remote_sigma.sh
+++ install_pc_remote_sigma.sh
@@ -63,33 +63,18 @@ echo $user_name:$pwd_user | sudo chpasswd
 echo "Change user to : $user_name, please type pwd of $user_name ?"
 su $user_name
 
-#Install turtlebot3 dependent packages
-cd
-mkdir -p turtlebot3/catkin_ws/src
-cd turtlebot3/catkin_ws/src
-git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
-git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
-git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
-cd ~/turtlebot3/catkin_ws && catkin_make
-
-cd ~/turtlebot3
-wget https://raw.githubusercontent.com/lequievre/turtlebot3/master/send_stop.sh
-wget https://raw.githubusercontent.com/lequievre/turtlebot3/master/set_ros_env_turtlebot3.sh
-wget https://raw.githubusercontent.com/lequievre/turtlebot3/master/set_ros_env_local_turtlebot3.sh
-
-echo "alias tb3_set_env_burger='source ~/turtlebot3/set_ros_env_turtlebot3.sh burger'" >> ~/.bashrc
-echo "alias tb3_set_env_waffle='source ~/turtlebot3/set_ros_env_turtlebot3.sh waffle'" >> ~/.bashrc
-echo "alias tb3_set_env_waffle_pi='source ~/turtlebot3/set_ros_env_turtlebot3.sh waffle_pi'" >> ~/.bashrc
-echo "alias tb3_set_env_local_burger='source ~/turtlebot3/set_ros_env_local_turtlebot3.sh burger'" >> ~/.bashrc
-echo "alias tb3_set_env_local_waffle='source ~/turtlebot3/set_ros_env_local_turtlebot3.sh waffle'" >> ~/.bashrc
-echo "alias tb3_set_env_local_waffle_pi='source ~/turtlebot3/set_ros_env_local_turtlebot3.sh waffle_pi'" >> ~/.bashrc
-echo "alias tb3_bringup='source ~/turtlebot3/catkin_ws/devel/setup.bash && roslaunch turtlebot3_bringup turtlebot3_remote.launch'" >> ~/.bashrc
-echo "alias tb3_rviz_model='source ~/turtlebot3/catkin_ws/devel/setup.bash && rosrun rviz rviz -d \`rospack find turtlebot3_description\`/rviz/model.rviz'" >> ~/.bashrc
-echo "alias tb3_rviz_markers='source ~/turtlebot3/catkin_ws/devel/setup.bash && rosrun rviz rviz -d \`rospack find turtlebot3_example\`/rviz/turtlebot3_interactive.rviz'" >> ~/.bashrc
-echo "alias tb3_markers='source ~/turtlebot3/catkin_ws/devel/setup.bash && roslaunch turtlebot3_example interactive_markers.launch'" >> ~/.bashrc
-echo "alias tb3_stop='source ~/turtlebot3/send_stop.sh'" >> ~/.bashrc
-echo "alias tb3_teleop='source ~/turtlebot3/catkin_ws/devel/setup.bash && roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch'" >> ~/.bashrc
-echo "alias tb3_print_env='echo \"ROS_MASTER_URI=\$ROS_MASTER_URI\" && echo \"ROS_HOSTNAME=\$ROS_HOSTNAME\" && echo \"TURTLEBOT3_MODEL=\$TURTLEBOT3_MODEL\"'" >> ~/.bashrc
-
-source ~/.bashrc
-
+#Install student
+echo "Please pwd of $user_name ?"
+su - "$user_name" -c "source /opt/ros/kinetic/setup.bash; sudo rosdep init; rosdep update; echo \"source /opt/ros/kinetic/setup.bash\" >> ~/.bashrc; echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc; echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc; source ~/.bashrc; cd ~; mkdir -p turtlebot3/catkin_ws/src; cd turtlebot3/catkin_ws/src; git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git; git clone https://github.com/ROBOTIS-GIT/turtlebot3.git; git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git; cd ~/turtlebot3/catkin_ws && catkin_make; cd ~/turtlebot3; wget https://raw.githubusercontent.com/lequievre/turtlebot3/master/send_stop.sh; wget https://raw.githubusercontent.com/lequievre/turtlebot3/master/set_ros_env_turtlebot3.sh; wget https://raw.githubusercontent.com/lequievre/turtlebot3/master/set_ros_env_local_turtlebot3.sh; echo \"alias tb3_set_env_burger='source ~/turtlebot3/set_ros_env_turtlebot3.sh burger'\" >> ~/.bashrc; 
+echo \"alias tb3_set_env_waffle='source ~/turtlebot3/set_ros_env_turtlebot3.sh waffle'\" >> ~/.bashrc; 
+echo \"alias tb3_set_env_waffle_pi='source ~/turtlebot3/set_ros_env_turtlebot3.sh waffle_pi'\" >> ~/.bashrc; 
+echo \"alias tb3_set_env_local_burger='source ~/turtlebot3/set_ros_env_local_turtlebot3.sh burger'\" >> ~/.bashrc; 
+echo \"alias tb3_set_env_local_waffle='source ~/turtlebot3/set_ros_env_local_turtlebot3.sh waffle'\" >> ~/.bashrc; 
+echo \"alias tb3_set_env_local_waffle_pi='source ~/turtlebot3/set_ros_env_local_turtlebot3.sh waffle_pi'\" >> ~/.bashrc; 
+echo \"alias tb3_bringup='source ~/turtlebot3/catkin_ws/devel/setup.bash && roslaunch turtlebot3_bringup turtlebot3_remote.launch'\" >> ~/.bashrc; 
+echo \"alias tb3_rviz_model='source ~/turtlebot3/catkin_ws/devel/setup.bash && rosrun rviz rviz -d \`rospack find turtlebot3_description\`/rviz/model.rviz'\" >> ~/.bashrc; 
+echo \"alias tb3_rviz_markers='source ~/turtlebot3/catkin_ws/devel/setup.bash && rosrun rviz rviz -d \`rospack find turtlebot3_example\`/rviz/turtlebot3_interactive.rviz'\" >> ~/.bashrc; 
+echo \"alias tb3_markers='source ~/turtlebot3/catkin_ws/devel/setup.bash && roslaunch turtlebot3_example interactive_markers.launch'\" >> ~/.bashrc; 
+echo \"alias tb3_stop='source ~/turtlebot3/send_stop.sh'\" >> ~/.bashrc; 
+echo \"alias tb3_teleop='source ~/turtlebot3/catkin_ws/devel/setup.bash && roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch'\" >> ~/.bashrc; 
+echo \"alias tb3_print_env='echo \"ROS_MASTER_URI='\$ROS_MASTER_URI'\" && echo \"ROS_HOSTNAME='\$ROS_HOSTNAME'\" && echo \"TURTLEBOT3_MODEL='\$TURTLEBOT3_MODEL'\"'\" >> ~/.bashrc; source ~/.bashrc"
